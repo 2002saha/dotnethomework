d9d943bc982a04741d2832548aaadac1abe15c24
alexanderpolez <maks310343@icloud.com>
2022-10-06 00:13:54 +0300

    Выполнено 15 задание

---
+++ Lesson2/homework/task015/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введи цифру, обозначающую день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day == 6 || day == 7) {
+  Console.WriteLine("(этот день выходной) -> да");
+  }
+else if (day < 1 || day > 7) {
+    Console.WriteLine("(ошибка) -> введите число от 1 до 7");
+  }
+else Console.WriteLine("(этот день не выходной) -> нет");
+
+
