dfa6263d6bc94ba42411c8ca2da981baced81765
alexanderpolez <maks310343@icloud.com>
2022-10-05 17:37:16 +0300

    8 задача

---
+++ Lesson01/homework/task008/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+Console.WriteLine("Start");
+ 
+for (int x = 2; x < 5; x++)       
+if (x%2==0)            
+
+Console.WriteLine(x);
+
+Console.WriteLine("End");
