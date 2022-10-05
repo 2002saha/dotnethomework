db728d07a3da0bd63719393e1409ba42d975a122
alexanderpolez <maks310343@icloud.com>
2022-10-05 16:43:19 +0300

    Решено задание 2 из домашней работы

---
+++ Lesson01/homework/task002/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+Console.WriteLine("Start");
+// Введите два числа после a = , b = 
+int a = 5;
+int b = 7;
+int max = a; 
+
+
+if (b > max)
+{
+    max = b;
+}
+
+Console.WriteLine ("Максимальное значение - " + max);
+
+Console.WriteLine ("End");
