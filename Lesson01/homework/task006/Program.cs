71f7dc8a995808906828e7a2e917efa63f90f471
alexanderpolez <maks310343@icloud.com>
2022-10-05 17:25:27 +0300

    6 задание

---
+++ Lesson01/homework/task006/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+Console.WriteLine("Start");
+// Введите два числа после a =
+int a = 4;
+ 
+ if (a % 2 == 0)
+            {
+                Console.WriteLine("четное число");
+           
+            }
+            else
+            {
+                Console.WriteLine("нечетное");
+            }
+ Console.WriteLine ("End");
