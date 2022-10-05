dd5831e9d1d6578721a2eb1cdcb987e57d582b28
alexanderpolez <maks310343@icloud.com>
2022-10-05 23:46:23 +0300

    Задание 10 выполнено

---
+++ Lesson2/homework/task010/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    // Введем трехзначное число
+Console.WriteLine("Введите трехзначное число");
+
+    //Наглядно выводит вторую цифру
+string input = Console.ReadLine();
+Console.WriteLine("Ваше число {0} -> Вторая цифра {1}",input, input[1]);
+
