c28c76be38cff8b650b76284871e5d21e2a9a0b4
alexanderpolez <maks310343@icloud.com>
2022-10-09 13:01:21 +0300

    19 Задание выполнено

---
+++ Lesson3/homework/task019/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Вводим пятизначное число 
+Console.Write("Введите ваше число: ");
+//Указываем что введеное число это массив и присваеваем его Number
+string number = Console.ReadLine();
+
+void CheckNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine($"Ваше число: {number} - палиндром.");
+  }
+  else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+}
+//Проверка количества символов 
+if (number!.Length == 5){
+ CheckNumber(number);
+}
+else Console.WriteLine($" (Ошибка 01) Число {number} не пятизначное");
