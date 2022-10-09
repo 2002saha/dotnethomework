1eefdefa3787043a3e2f9412d4fe4de5d667dbc6
alexanderpolez <maks310343@icloud.com>
2022-10-09 13:02:52 +0300

    Фикс

--- Lesson2/homework/task013/Program.cs
+++ Lesson2/homework/task013/Program.cs
@@ -12,9 +12,9 @@ Console.WriteLine ("Нахождения третей цирфы в числе")
 Console.WriteLine("Введите число");
 
 //Наглядно выводит третью цифру
-string input = Console.ReadLine();
+string? input = Console.ReadLine();
 
-if (input.Length > 2){
+if (input!.Length > 2){
     Console.WriteLine("Ваше число {0} -> Третья цифра {1}",input, input[2]);
 }
 // Если третей цифры нет, то мы выводим текст об этом
