6031812e6f56e3e15221674f0ab47ed01126176a
alexanderpolez <maks310343@icloud.com>
2022-10-09 13:02:00 +0300

    Фикс Null

--- Lesson2/homework/task010/Program.cs
+++ Lesson2/homework/task010/Program.cs
@@ -9,6 +9,6 @@ using System.Threading.Tasks;
 Console.WriteLine("Введите трехзначное число");
 
     //Наглядно выводит вторую цифру
-string input = Console.ReadLine();
-Console.WriteLine("Ваше число {0} -> Вторая цифра {1}",input, input[1]);
+string? input= Console.ReadLine();
+Console.WriteLine("Ваше число {0} -> Вторая цифра {1}",input, input![1]);
 
