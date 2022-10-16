8b9fef525b2372e3d9c201ee1e49cdf94b617000
alexanderpolez <maks310343@icloud.com>
2022-10-16 16:20:01 +0300

    Что-то изменилось

--- Lesson4/homework/task029/Program.cs
+++ Lesson4/homework/task029/Program.cs
@@ -9,20 +9,20 @@ Console.WriteLine("\n Вывод массива на экран");
 
 Console.Write("Введите количество элементов массива:\t");
 {
-   
-     
-     int elementsCount = int.Parse(Console.ReadLine());
 
-     int[] myArray =new int[elementsCount];
-     
-     for (int i = 0; i < myArray.Length; i++)
-     {
-        Console.Write ($"Введите элемент массива под индексом {i}:\t ");
-        myArray[i] = int.Parse(Console.ReadLine()); 
-     }
-      
-      Console.WriteLine("\n Вывод массива: ");
-      
-      Console.WriteLine("[{0}]", string.Join(", ", myArray));
-      
+
+    int elementsCount = int.Parse(Console.ReadLine());
+
+    int[] myArray = new int[elementsCount];
+
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        Console.Write($"Введите элемент массива под индексом {i}:\t ");
+        myArray[i] = int.Parse(Console.ReadLine());
+    }
+
+    Console.WriteLine("\n Вывод массива: ");
+
+    Console.WriteLine("[{0}]", string.Join(", ", myArray));
+
 }
