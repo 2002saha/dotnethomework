a9a5241ba943c071db6681d931bdffbcbced09e7
alexanderpolez <maks310343@icloud.com>
2022-10-16 17:05:36 +0300

    Фикс

--- Lesson4/homework/task029/Program.cs
+++ Lesson4/homework/task029/Program.cs
@@ -11,14 +11,14 @@ Console.Write("Введите количество элементов масси
 {
 
 
-    int elementsCount = int.Parse(Console.ReadLine());
+    int elementsCount = int.Parse(Console.ReadLine()!);
 
     int[] myArray = new int[elementsCount];
 
     for (int i = 0; i < myArray.Length; i++)
     {
         Console.Write($"Введите элемент массива под индексом {i}:\t ");
-        myArray[i] = int.Parse(Console.ReadLine());
+        myArray[i] = int.Parse(Console.ReadLine()!);
     }
 
     Console.WriteLine("\n Вывод массива: ");
