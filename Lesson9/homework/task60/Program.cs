3921af8dda8b3fda0358639f784cf1d86b3ed8a0
alexanderpolez <maks310343@icloud.com>
2022-11-18 11:24:58 +0300

    Задача 66 сделана

---
+++ Lesson9/homework/task60/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.BackgroundColor = ConsoleColor.White;
+Console.WriteLine($"Генератор трехмерного массива со случайными неповторяющиемеся числами");
+Console.ResetColor();
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\nВведите размеры массива через пробел (например: 2 2 2): ");
+Console.ResetColor();
+string[] nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int[,,] array = GetArray(new int[] { int.Parse(nums[0]), int.Parse(nums[1]), int.Parse(nums[2]), }, 10, 99);
+
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine($"\nСгенерированный массив: ");
+Console.ResetColor();
+
+PrintArray(array);
+Console.ResetColor();
+Console.ReadKey();
+Console.Clear();
+
+int[,,] GetArray(int[] sizes, int min, int max)
+{
+    int[,,] result = new int[sizes[0], sizes[1], sizes[2]];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            int k = 0;
+            while (k < result.GetLength(2))
+            {
+                int element = new Random().Next(min, max + 1);
+                if (FindElement(result, element)) continue;
+                result[i, j, k] = element;
+                k++;
+            }
+        }
+    }
+    return result;
+}
+
+bool FindElement(int[,,] array, int el)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                if (array[i, j, k] == el) return true;
+            }
+        }
+    }
+    return false;
+}
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]} ({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Lesson9/homework/task66/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+Console.ResetColor();
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.BackgroundColor = ConsoleColor.White;
+Console.WriteLine($"\nСложение натуральных чисел в промежутке от M до N");
+Console.ResetColor();
+Console.WriteLine();
+
+int InputNumber(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void PrintSum(int m, int n, int sum)
+{
+    sum = sum + n;
+    if (n <= m)
+    {
+        Console.Write($" {sum} ");
+        return;
+    }
+    PrintSum(m, n - 1, sum);
+
+}
+int m = InputNumber("Введите число M: ");
+int n = InputNumber("Введите число N: ");
+int temp = m;
+
+if (m > n)
+{
+    m = n;
+    n = temp;
+}
+
+Console.Clear();
+Console.BackgroundColor = ConsoleColor.Green;
+Console.WriteLine($"Cумма натуральных элементов в промежутке от {m} до {n}:");
+Console.WriteLine();
+Console.ResetColor();
+Console.Write($"{m}, {n} ->");
+PrintSum(m, n, temp = 0);
+
+Console.WriteLine();
+Console.ReadKey();
+Console.Clear();
